d898ca2bac9e954c768be5ef35c3e261abc82d6c
tjsdavies <tjsdavies@gmail.com>
2017-06-26 09:23:48 +0100

    todo app

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,24 +4,40 @@ import RDXReact from './RDXReact';
 //reducer
 const init = s => ({
   idCount: 0,
-  todos:[]
+  todos:[],
+  filter: "SHOW_ALL"
 })
 
-const addTodo = s => ({
+const addTodo = text => s => ({
   idCount: s.idCount + 1,
-  todos: [...s.todos, {id:s.idCount + 1, title:"", body:""} ]
+  todos: [...s.todos, {id:s.idCount + 1, text:text, completed:false} ]
 })
 
-const removeTodo = todo => s => ({
+const compleatTodo = id => s => ({
   ...s,
-  todos: s.todos.filter( t => t.id !== todo.id )
+  todos: s.todos.map( markCompleated(id) )
 })
+
+const markCompleated = id => t => {
+  if(t.id === id){
+    t.completed = !t.completed ;
+  }
+  return t;
+}
+
+const setFilter = filter => s => ({
+  ...s,
+  filter: filter
+})
+
+
 
 //actions
 const actions = {
   initalise: a => a.map(init),
-  addTodo: a => a.map(addTodo),
-  removeTodo: (a,todo) => a.map(removeTodo(todo)),
+  addTodo: (a,text) => a.map(addTodo(text)).log(),
+  compleatTodo: (a, id) => a.map(compleatTodo(id)),
+  setFilter: (a, filter) => a.map(setFilter(filter))
 }
 
 export const TodoApp = RDXReact(view, actions);
