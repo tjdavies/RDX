98f49e62ddc5535f7b54d2d5badda64f852eb651
tjsdavies <tjsdavies@gmail.com>
2017-07-21 11:52:41 +0100

    added redux dev tools support

--- src/copal.js
+++ src/copal.js
@@ -1,18 +1,55 @@
-export function createStore(rawActions, update){
+export function createStore(rawActions, middleware = (f) => f  ){
     let state = {};
     let actions = {};
+    let currentListeners = []
+    let nextListeners = currentListeners
 
-    const makeAction = (a,name) => (...args) => {
-      const response = a(action({ state, effects: [] },...args),...args).fold();
-      state = response.state
+    const dispatch = (name, ...args) => {
+      const response = rawActions[name](action({ state, effects: [] },...args),...args).fold();
+      setState(response.state);
       response.effects.map(e => e(response.state, actions));
-      update(state, actions)
-    };
-
-    for (var key in rawActions) {
-        actions[key] = makeAction(rawActions[key],key);
+      return state;
     }
-    return actions;
+
+    const makeAction = (name) => (...args) => middleware(dispatch)(name, ...args)
+    
+    for (var key in rawActions) {
+      actions[key] = makeAction(key);
+    }
+
+    const getState = () => {
+      return state
+    }
+
+    const setState = (newState) => {
+      state = newState
+      const listeners = currentListeners = nextListeners
+      for (let i = 0; i < listeners.length; i++) {
+        const listener = listeners[i]
+        listener()
+      }
+    }
+  
+    function subscribe(listener) {
+      let isSubscribed = true
+      nextListeners.push(listener)
+      return function unsubscribe() {
+        if (!isSubscribed) {
+          return
+        }
+        isSubscribed = false
+        const index = nextListeners.indexOf(listener)
+        nextListeners.splice(index, 1)
+      }
+    }
+   
+    return {
+      actions,
+      getState,
+      subscribe,
+      dispatch,
+      setState
+    };
 }
 
 function action(s,...args) {
--- src/copalReact.js
+++ src/copalReact.js
@@ -5,17 +5,38 @@ class CopalComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.actions = createStore(props.actions, s => this.setState(s) );
+
+    var devTools = window.devToolsExtension && window.devToolsExtension.connect();
+    let debuggerHook = (dispatch) => (name,...args) => {
+      const newState = dispatch(name, ...args)
+      devTools.send({type:name,...args}, newState)
+    }
+    this.store = createStore(props.actions, (devTools) ? debuggerHook : undefined);
+    if(devTools){
+      this.unsubscribe = devTools.subscribe( (message) => {
+        if(message.type === 'DISPATCH'){
+          this.store.setState(JSON.parse(message.state));
+        }
+      })
+      devTools.init(this.store.getState());
+    }  
+    this.store.subscribe(
+      () => this.setState(this.store.getState())
+    )
   }
 
   componentWillMount() {
-    if (this.actions['initalise']){
-      this.actions['initalise']();
+    if (this.store.actions['initalise']){
+      this.store.actions['initalise']();
     }
   }
 
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
-    return this.props.render(this.state, this.actions);
+    return this.props.render(this.state, this.store.actions);
   }
 }
 
