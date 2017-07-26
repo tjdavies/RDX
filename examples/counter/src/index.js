c994904aa1ed95514f521d79dc2e83b783862169
tjsdavies <tjsdavies@gmail.com>
2017-07-26 15:33:41 +0100

    added counter example

---
+++ examples/counter/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import copal from 'copal';
+
+export const TodoApp = copal(
+    (state, actions) =>
+        <div>
+            <button onClick={actions.decrement}>-</button>
+            <div>{state.value}</div>
+            <button onClick={actions.increment}>+</button>
+        </div>
+    ,  
+    {
+        initalise: a => a.map( s => ({value: 0})),
+        increment: a => a.map( s => ({value: s.value + 1})),
+        decrement: a => a.map( s => ({value: s.value - 1})),
+    }
+)
+
+ReactDOM.render(TodoApp, document.getElementById('root'));
